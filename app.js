9e83d25e9ece57aff8278331949a57bf85f3dca7
Alkawe <alixkwebb@gmail.com>
2020-09-30 00:24:23 -0400

    cleaned up and added catch functions after then functions

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// array that will contain all employee objects
 const teamArray = [];
 
 promptUser();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Write code to use inquirer to gather information about the development team manager,
 function promptUser() {
     inquirer.prompt([
         {
@@ -30,7 +30,18 @@ function promptUser() {
         {
             type: "input",
             name: "email",
-            message: "What is your manager's email?"
+            message: "What is your manager's email?",
+            validate: function(email) {
+                if(email === "" || email === null || email === undefined) {
+                    console.log("\nPlease enter valid email");
+                }
+                else if(email.includes("@") === false || email.includes(".com") === false) {
+                    console.log("\nPlease enter valid email");
+                }
+                else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
@@ -46,18 +57,12 @@ function promptUser() {
     ])
     .then(response => {
         const manager = new Manager(response.name, response.id, response.email, response.number);
-        console.log("Employee Added");
         teamArray.push(manager);
         buildTeam(response.teamMember);
     }).catch((err) => {console.log(err)});
 }
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
+// this code is written to ask different questions via inquirer depending on employee type.
 function buildTeam(response) {
     if (response === "Engineer") {
         inquirer.prompt ([
@@ -74,7 +79,18 @@ function buildTeam(response) {
         {
             type: "input",
             name: "email",
-            message: "What is your engineer's email?"
+            message: "What is your engineer's email?",
+            validate: function(email) {
+                if(email === "" || email === null || email === undefined) {
+                    console.log("\nPlease enter valid email");
+                }
+                else if(email.includes("@") === false || email.includes(".com") === false) {
+                    console.log("\nPlease enter valid email");
+                }
+                else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
@@ -89,10 +105,9 @@ function buildTeam(response) {
         }
         ]).then(function(response) {
             const engineer = new Engineer(response.name, response.id, response.email, response.github);
-            console.log("Employee Added");
             teamArray.push(engineer);
             buildTeam(response.teamMember);
-        });
+        }).catch((err) => {console.log(err)});
     } else if (response === "Intern") {
         inquirer.prompt ([
         {
@@ -106,7 +121,7 @@ function buildTeam(response) {
             message: "What is your intern's id?"
         },
         {
-            // type: "input",
+            type: "input",
             name: "email",
             message: "What is your intern's email?",
             validate: function(email) {
@@ -134,31 +149,17 @@ function buildTeam(response) {
         }
     ]).then(function(response) {
         const intern = new Intern(response.name, response.id, response.email, response.school);
-        console.log("Employee Added");
         teamArray.push(intern);
         buildTeam(response.teamMember);
-    });
+    }).catch((err) => {console.log(err)});
+    // After the user has input all employees desired, call the `render` function
+    // The render function will generate and return a block of HTML including templated divs for each employee!
+    // create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the
+    // `output` folder. Use the variable `outputPath` above target this location.
     } else {
-        // const renderEmployees = render(team);
         fs.writeFile(outputPath, render(teamArray), function(err) {
         if (err) throw err;
-        console.log("It worked!");
+        console.log("All employees have been added.");
         });
     }
 }
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
