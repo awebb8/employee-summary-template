d58931ac27a6eaf3474f47e3a5b13fff99e20a92
Alkawe <alixkwebb@gmail.com>
2020-09-29 21:25:55 -0400

    added code for buildTeam function and modified promptUser function

--- app.js
+++ app.js
@@ -10,13 +10,142 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamArray = [];
 
+promptUser();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your manager's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your manager's email?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is your manager's office number?",
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Which type of team member would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+        }
+    ])
+    .then(response => {
+        const manager = new Manager(response.name, response.id, response.email, response.number);
+        console.log("Employee Added");
+        teamArray.push(manager);
+        buildTeam(response.teamMember);
+    }).catch((err) => {console.log(err)});
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
+function buildTeam(response) {
+    if (response === "Engineer") {
+        inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineer's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's GitHub username?"
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Which type of team member would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+        }
+        ]).then(function(response) {
+            const engineer = new Engineer(response.name, response.id, response.email, response.github);
+            console.log("Employee Added");
+            teamArray.push(engineer);
+            buildTeam(response.teamMember);
+        });
+    } else if (response === "Intern") {
+        inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your intern's id?"
+        },
+        {
+            // type: "input",
+            name: "email",
+            message: "What is your intern's email?",
+            validate: function(email) {
+                if(email === "" || email === null || email === undefined) {
+                    console.log("\nPlease enter valid email");
+                }
+                else if(email.includes("@") === false || email.includes(".com") === false) {
+                    console.log("\nPlease enter valid email");
+                }
+                else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is your intern's school?"
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Which type of team member would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+        }
+    ]).then(function(response) {
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        console.log("Employee Added");
+        teamArray.push(intern);
+        buildTeam(response.teamMember);
+    });
+    } else {
+        // const renderEmployees = render(team);
+        fs.writeFile(outputPath, render(teamArray), function(err) {
+        if (err) throw err;
+        console.log("It worked!");
+        });
+    }
+}
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
